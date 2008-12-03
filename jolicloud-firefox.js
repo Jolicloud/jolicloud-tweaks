82371a501296c0a1b71065dfcfd9ad1181f1bc16
Tristan Groléat <tristan@jolicloud.org>
2008-12-03 14:19:12 +0000

    jolicloud-tweaks sets default settings for firefox
    git-svn-id: http://dev.jolicloud.org/svn/jolicloud/system/jolicloud-tweaks/trunk@11 530a43d6-6355-46e3-abf2-ead6c96053c9

---
+++ jolicloud-firefox.js
@@ -0,0 +1,4 @@
+// Jolicloud specific settings for firefox
+
+pref("browser.startup.homepage", "file:/usr/share/jolicloud-tweaks/jolicloud-firefox.properties");
+pref("browser.download.dir", "~/Downloads");
