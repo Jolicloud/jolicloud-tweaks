586abd9aa206a4eb03bd6e76307a0f842fe34077
Tristan Groléat <tristan@jolicloud.org>
2009-04-27 18:41:03 +0200

    Fixed firefox preferences for the default download directory (ticket #37)

--- jolicloud-firefox.js
+++ jolicloud-firefox.js
@@ -2,3 +2,4 @@
 
 pref("browser.startup.homepage", "file:/usr/share/jolicloud-tweaks/jolicloud-firefox.properties");
 pref("browser.download.dir", "~/Downloads");
+pref("browser.download.folderList", 2);
