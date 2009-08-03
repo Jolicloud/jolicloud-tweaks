a1484ff9e5ca6d329c15d5758573469bdfd4782f
Benjamin Ryzman <zarkdav@jolidell.(none)>
2009-08-03 19:58:16 +0200

    Try some firefox optimisations

--- jolicloud-firefox.js
+++ jolicloud-firefox.js
@@ -3,3 +3,15 @@
 pref("browser.startup.homepage", "file:/usr/share/jolicloud-tweaks/jolicloud-firefox.properties");
 pref("browser.download.dir", "~/Downloads");
 pref("browser.download.folderList", 2);
+pref("network.http.pipelining", true);
+pref("network.http.proxy.pipelining", true);
+pref("network.http.pipelining.maxrequests", 15);
+pref("network.dns.disableIPv6", true);
+pref("nglayout.initialpaint.delay", 0);
+pref("ui.submenuDelay", 0);
+pref("content.notify.backoffcount", 5);
+pref("network.prefetch-next", false);
+pref("general.smoothScroll", true);
+pref("general.autoScroll", true);
+pref("toolkit.storage.synchronous", 0);
+pref("browser.cache.memory.capacity", 16384);
